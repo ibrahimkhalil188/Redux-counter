9f793528a86a2a647b9ab259d97bb77b2445d827
unknown <ibrahimkhalil.mine@gmail.com>
2022-06-24 00:06:58 +0600

    other funtionality added

--- src/Component/Counter.js
+++ src/Component/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux/es/exports';
-import { counterIncrement } from '../Services/Action/Action';
+import { counterDecrement, counterIncrement, counterReset } from '../Services/Action/Action';
 const Counter = () => {
     const count = useSelector((state) => state.count)
     const dispatch = useDispatch()
@@ -10,6 +10,8 @@ const Counter = () => {
             <h2 >This is Counter APP</h2>
             <h3>Count:{count}</h3>
             <button onClick={() => dispatch(counterIncrement())}>Increment</button>
+            <button onClick={() => dispatch(counterDecrement())}>Decrement</button>
+            <button onClick={() => dispatch(counterReset())}>Reset</button>
         </div>
     );
 };
